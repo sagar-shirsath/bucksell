0476986f485d9198e652f797d374c33255f70611
sagar <sagar.shirsath2009@gmail.com>
2012-09-22 23:46:54 +0530

    install app

--- main/bucksell/settings.py
+++ main/bucksell/settings.py
@@ -118,7 +118,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'ads',
-    'registrations',
+    'registration',
     'categories',
     'feedbacks',
     'messages',

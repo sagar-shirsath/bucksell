c1cc3c354ddb654a9ad88db3d9bcfbc247ae063c
sagar <sagar.shirsath2009@gmail.com>
2012-09-23 18:17:56 +0530

    home page created

--- main/bucksell/apps/user_profiles/views.py
+++ main/bucksell/apps/user_profiles/views.py
@@ -1,1 +1,13 @@
-# Create your views here.
+from django.contrib.auth.models import User
+from django.shortcuts import render_to_response
+from django.http import HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login , authenticate, logout
+from django.core.urlresolvers import reverse
+from django.template import RequestContext
+from django.shortcuts import get_object_or_404
+from datetime import date
+
+
+def home(request):
+    return render_to_response("user_profiles/home.html" ,{} ,context_instance = RequestContext(request))
--- main/bucksell/urls.py
+++ main/bucksell/urls.py
@@ -10,7 +10,7 @@ urlpatterns = patterns('',
     # Examples:
     #    url(r'^$', 'test1.views.home', name='home'),
     # url(r'^test1/', include('test1.foo.urls')),
-
+    url(r'^$','user_profiles.views.home',{},name = "home"),
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 

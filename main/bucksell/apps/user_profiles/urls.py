c52ed91b0e0e6c9fc2dd19b3f7f44d004fd78039
sagar <sagar.shirsath2009@gmail.com>
2012-09-29 22:35:17 +0530

    login with email

--- main/bucksell/apps/user_profiles/urls.py
+++ main/bucksell/apps/user_profiles/urls.py
@@ -3,6 +3,9 @@ from django.conf.urls.defaults import *
 from django.contrib.auth import views as auth_views
 from user_profiles import views
 urlpatterns = patterns('',
+    url(r'^login/$',
+        views.login_me,
+        name='auth_login'),
     url(r'^edit_profile/$',
         views.edit_profile,
         name='edit_profile'),
--- main/bucksell/external_apps/registration/auth_urls.py
+++ main/bucksell/external_apps/registration/auth_urls.py
@@ -26,13 +26,10 @@ consult a specific backend's documentation for details.
 from django.conf.urls.defaults import *
 
 from django.contrib.auth import views as auth_views
-
+from forms import LoginForm
 
 urlpatterns = patterns('',
-                       url(r'^login/$',
-                           auth_views.login,
-                           {'template_name': 'registration/login.html','redirect_field_name':'/'},
-                           name='auth_login'),
+
                        url(r'^logout/$',
                            auth_views.logout,
                            {'template_name': 'registration/logout.html','next_page':'/'},

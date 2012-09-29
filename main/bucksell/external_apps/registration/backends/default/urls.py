9023a80a94b95f7fed66c8aeacbb1057713565f4
sagar <sagar.shirsath2009@gmail.com>
2012-09-29 10:34:54 +0530

    account activation

--- main/bucksell/external_apps/registration/backends/default/urls.py
+++ main/bucksell/external_apps/registration/backends/default/urls.py
@@ -36,7 +36,7 @@ urlpatterns = patterns('',
                        # confusing 404.
                        url(r'^activate/(?P<activation_key>\w+)/$',
                            activate,
-                           {'backend': 'registration.backends.default.DefaultBackend'},
+                           {'backend': 'registration.backends.default.DefaultBackend','success_url':'/'},
                            name='registration_activate'),
                        url(r'^register/$',
                            register,
--- main/bucksell/external_apps/registration/views.py
+++ main/bucksell/external_apps/registration/views.py
@@ -79,6 +79,7 @@ def activate(request, backend,
             to, args, kwargs = backend.post_activation_redirect(request, account)
             return redirect(to, *args, **kwargs)
         else:
+            request.flash['message'] = 'Your account has been successfully activated you can login now.'
             return redirect(success_url)
 
     if extra_context is None:

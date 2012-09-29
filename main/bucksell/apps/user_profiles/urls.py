c13df573645008968c522e8a5867e129209a70b7
sagar <sagar.shirsath2009@gmail.com>
2012-09-29 21:05:38 +0530

    new landing page inegrated

--- main/bucksell/apps/user_profiles/urls.py
+++ main/bucksell/apps/user_profiles/urls.py
@@ -1,15 +1,16 @@
 from django.conf.urls.defaults import *
 
 from django.contrib.auth import views as auth_views
-
-
+from user_profiles import views
 urlpatterns = patterns('',
-#    url(r'^login/$',
-#        auth_views.login,
-#            {'template_name': 'registration/login.html'},
-#        name='auth_login'),
-#    url(r'^register/$',
-#        auth_views.logout,
-#            {'template_name': 'registration/registration_form.html'},
-#        name='register'),
+    url(r'^edit_profile/$',
+        views.edit_profile,
+        name='edit_profile'),
+    url(r'^edit_contacts/$',
+        views.edit_contacts,
+        name='edit_contacts'),
+    url(r'^edit_security/$',
+        views.edit_security,
+        name='edit_security'),
+
 )
--- main/bucksell/urls.py
+++ main/bucksell/urls.py
@@ -11,6 +11,7 @@ urlpatterns = patterns('',
     #    url(r'^$', 'test1.views.home', name='home'),
     # url(r'^test1/', include('test1.foo.urls')),
     url(r'^$','user_profiles.views.home',{},name = "home"),
+    url(r'^user/',include('user_profiles.urls'),{},name = "home"),
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 

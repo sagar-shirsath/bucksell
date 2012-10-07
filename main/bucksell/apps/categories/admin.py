d716a37329e85c025da28d3723007e2309ebcd5e
sagar <sagar.shirsath2009@gmail.com>
2012-10-07 17:10:10 +0530

    category and university management modified

--- main/bucksell/apps/categories/admin.py
+++ main/bucksell/apps/categories/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from categories.models import Category
-#class CategoryAdmin(admin.ModelAdmin):
-
-admin.site.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display= ['name']
+admin.site.register(Category,CategoryAdmin)
 
 
 

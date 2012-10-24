586201ab1f84a62291d935af2fa2be1d1cd0e29e
sagar <sagar.shirsath2009@gmail.com>
2012-10-24 09:33:14 +0530

    photo upload working

--- main/bucksell/apps/items/views.py
+++ main/bucksell/apps/items/views.py
@@ -9,10 +9,11 @@ from django.shortcuts import get_object_or_404
 from datetime import date
 from items.forms import ItemForm
 from items.models import Item, ItemPhoto
-from user_profiles.views import get_user_profile,is_image
+from user_profiles.views import get_user_profile,is_image ,handle_uploaded_image
 from categories.models import Category
-
-
+from django.conf import settings
+import os
+from time import time
 
 @login_required
 def index(request):
@@ -24,11 +25,48 @@ def my_listing(request):
     items = Item.objects.all()
     return render_to_response("items/my_listing.html", {'items':items}, context_instance=RequestContext(request))
 
-def upload_item_images(image1,image2,image3,item_id):
-    img_obj = ItemPhoto(item=item_id)
-    img_obj.photo.save(image1)
+def upload_item_images(image1,image2,image3,item):
+    path = settings.MEDIA_ROOT+"/images/items/"
+    photo_name1 = ("%d_%s_1.%s"%(item.id,("%s"%time())[1:6],"jpg"))
+    photo_name2 = ("%d_%s_2.%s"%(item.id,("%s"%time())[1:6],"jpg"))
+    photo_name3 = ("%d_%s_3.%s"%(item.id,("%s"%time())[1:6],"jpg"))
+    photo_thumbnail_path1 = ("%d_thumbnail_%s_1.%s"%(item.id,("%s"%time())[1:6],item.slug))
+    photo_thumbnail_path2= ("%d_%s_2.%s"%(item.id,("%s"%time())[1:6],item.slug))
+    photo_thumbnail_path3= ("%d_%s_3.%s"%(item.id,("%s"%time())[1:6],item.slug))
+
+    img_obj,created = ItemPhoto.objects.get_or_create(item=item)
+    if(img_obj.photo1):
+        img_obj.photo1.delete()
+    photo1 = img_obj.photo1.save(photo_name1,image1)
+    if(img_obj.photo2):
+        img_obj.photo2.delete()
+    photo2 = img_obj.photo2.save(photo_name2,image2)
+    if(img_obj.photo3):
+        img_obj.photo3.delete()
+    photo3 = img_obj.photo3.save(photo_name3,image3)
+    size = (50,50)
+    img1 = open(path+photo_name1, 'r+')
+    img2 = open(path+photo_name2, 'r+')
+    img3 = open(path+photo_name3, 'r+')
+
+
+    thumb1 = handle_uploaded_image(img1,size)
+    thumb2 = handle_uploaded_image(img2,size)
+    thumb3 = handle_uploaded_image(img3,size)
+    if(img_obj.thumbnail1):
+        img_obj.thumbnail1.delete()
+    img_obj.thumbnail1.save(photo_thumbnail_path1+'.'+thumb1[0].split('.')[1],thumb1[1])
+    if(img_obj.thumbnail1):
+        img_obj.thumbnail1.delete()
+
+    img_obj.thumbnail2.save(photo_thumbnail_path2+'.'+thumb2[0].split('.')[1],thumb2[1])
+    if(img_obj.thumbnail1):
+        img_obj.thumbnail1.delete()
+
+    img_obj.thumbnail3.save(photo_thumbnail_path3+'.'+thumb3[0].split('.')[1],thumb3[1])
+
     img_obj.save()
-    return True
+    return img_obj
 @login_required
 def add(request):
 #    user, item = get_user_profile(request.user.id)
@@ -60,9 +98,9 @@ def add(request):
             upload_item_images(image1,image2,image3,item)
             request.flash['message'] = "Item saved successfully"
 
-#            else:
-#                request.flash['message'] = "Sorry can't save item"
-#                return HttpResponseRedirect(reverse('add_item'))
+        #            else:
+        #                request.flash['message'] = "Sorry can't save item"
+            return HttpResponseRedirect(reverse('my_listing'))
         else:
             request.flash['message'] = "Invalid data"
 
@@ -115,4 +153,4 @@ def edit(request,slug=""):
 
 def view(request):
 
-	return render_to_response("items/views.html", {}, context_instance=RequestContext(request))
+    return render_to_response("items/views.html", {}, context_instance=RequestContext(request))
